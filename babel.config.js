a24947a330977d5b71c5d69a41d8493b550fd7de
Conan Kunhwan Ahn <kunhwon@gmail.com>
2020-01-12 12:46:48 +0900

    init commit

---
+++ babel.config.js
@@ -0,0 +1,24 @@
+module.exports = api => {
+  api.cache(true)
+
+  const presets = [
+    [
+      '@babel/env',
+      {
+        targets: {
+          edge: '17',
+          firefox: '60',
+          chrome: '67',
+          safari: '11.1',
+        },
+        useBuiltIns: 'usage',
+        corejs: 3,
+      },
+    ],
+    ['minify', {}],
+  ]
+
+  return {
+    presets,
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,4 @@
+;(async () => {
+  // eslint-disable-next-line no-console
+  await console.log('Hello world!')
+})()
